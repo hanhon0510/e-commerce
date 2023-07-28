1aa42f2a32b1548de184866e1b0b6d80f865b8a9
hanhon0510 <tranhieuams@gmail.com>
2023-07-28 23:21:01 +0700

    create review section

--- src/customer/components/ProductDetails/ProductDetails.jsx
+++ src/customer/components/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
 import { RadioGroup } from "@headlessui/react";
+import { Button, Grid, Rating } from "@mui/material";
+import ProductReviewCard from "./ProductReviewCard";
 
 const product = {
   name: "Basic Tee 6-Pack",
@@ -34,14 +36,10 @@ const product = {
     { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
   ],
   sizes: [
-    { name: "XXS", inStock: false },
-    { name: "XS", inStock: true },
     { name: "S", inStock: true },
     { name: "M", inStock: true },
     { name: "L", inStock: true },
     { name: "XL", inStock: true },
-    { name: "2XL", inStock: true },
-    { name: "3XL", inStock: true },
   ],
   description:
     'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
@@ -155,85 +153,20 @@ export default function ProductDatils() {
 
               {/* Reviews */}
               <div className="mt-6">
-                <h3 className="sr-only">Reviews</h3>
-                <div className="flex items-center">
-                  <div className="flex items-center">
-                    {[0, 1, 2, 3, 4].map((rating) => (
-                      <StarIcon
-                        key={rating}
-                        className={classNames(
-                          reviews.average > rating
-                            ? "text-gray-900"
-                            : "text-gray-200",
-                          "h-5 w-5 flex-shrink-0"
-                        )}
-                        aria-hidden="true"
-                      />
-                    ))}
-                  </div>
-                  <p className="sr-only">{reviews.average} out of 5 stars</p>
-                  <a
-                    href={reviews.href}
-                    className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    {reviews.totalCount} reviews
-                  </a>
+                <div className="flex items-center scroll-px-3">
+                  <Rating name="read-only" value={5.5} readOnly />
+                  <p className="opacity-50 text-sm">56333 Ratings</p>
+                  <p className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                    222 Reviews
+                  </p>
                 </div>
               </div>
 
               <form className="mt-10">
-                {/* Colors */}
-                <div>
-                  <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                  <RadioGroup
-                    value={selectedColor}
-                    onChange={setSelectedColor}
-                    className="mt-4"
-                  >
-                    <RadioGroup.Label className="sr-only">
-                      Choose a color
-                    </RadioGroup.Label>
-                    <div className="flex items-center space-x-3">
-                      {product.colors.map((color) => (
-                        <RadioGroup.Option
-                          key={color.name}
-                          value={color}
-                          className={({ active, checked }) =>
-                            classNames(
-                              color.selectedClass,
-                              active && checked ? "ring ring-offset-1" : "",
-                              !active && checked ? "ring-2" : "",
-                              "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
-                            )
-                          }
-                        >
-                          <RadioGroup.Label as="span" className="sr-only">
-                            {color.name}
-                          </RadioGroup.Label>
-                          <span
-                            aria-hidden="true"
-                            className={classNames(
-                              color.class,
-                              "h-8 w-8 rounded-full border border-black border-opacity-10"
-                            )}
-                          />
-                        </RadioGroup.Option>
-                      ))}
-                    </div>
-                  </RadioGroup>
-                </div>
-
                 {/* Sizes */}
                 <div className="mt-10">
                   <div className="flex items-center justify-between">
                     <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                    <a
-                      href="#"
-                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Size guide
-                    </a>
                   </div>
 
                   <RadioGroup
@@ -305,12 +238,13 @@ export default function ProductDatils() {
                   </RadioGroup>
                 </div>
 
-                <button
-                  type="submit"
-                  className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  sx={{ px: "2rem", py: "1rem" }}
                 >
-                  Add to bag
-                </button>
+                  Add to cart
+                </Button>
               </form>
             </div>
 
@@ -350,6 +284,22 @@ export default function ProductDatils() {
                 </div>
               </div>
             </div>
+          </div>
+        </section>
+
+        {/* rating & reviews */}
+        <section>
+          <h1 className="font-semibold text-lg pb-4">
+            Recent reviews & ratings
+          </h1>
+          <div className="border p-5">
+            <Grid container spacing={7}>
+              <Grid item xs={7}>
+                <div className="space-y-5">
+                  <ProductReviewCard />
+                </div>
+              </Grid>
+            </Grid>
           </div>
         </section>
       </div>
---
+++ src/customer/components/ProductDetails/ProductReviewCard.jsx
@@ -0,0 +1,38 @@
+import { Avatar, Box, Grid, Rating } from "@mui/material";
+import React from "react";
+
+const ProductReviewCard = () => {
+  return (
+    <div>
+      <Grid container spacing={2} gap={3}>
+        <Grid item xs={1}>
+          <Box>
+            <Avatar
+              className="text-white"
+              sx={{ width: 56, height: 56, bgcolor: "#9155fd" }}
+            >
+              R
+            </Avatar>
+          </Box>
+        </Grid>
+        <Grid item xs={9}>
+          <div className="space-y-2">
+            <div>
+              <p>Hieu</p>
+              <p>11/11/1111</p>
+            </div>
+          </div>
+          <Rating value={4.5} name="half-rating" />
+          <p>
+            A radio group with its fields visually hidden. It contains six radio
+            buttons, one for each star, and another for 0 stars that is checked
+            by default. Be sure to provide a value for the name prop that is
+            unique to the parent form.
+          </p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ProductReviewCard;
